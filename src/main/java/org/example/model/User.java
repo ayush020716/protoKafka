c7e3d2d2480ee3ef90ae4a76f310bc7fdfd5b324
ayush.tiwari <ayush.tiwari@sprinklr.com>
2023-06-21 19:03:20 +0530

    create dummy model

---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,29 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class User {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String age;
+    private String password;
+    private String address;
+    private String phone;
+    private String bio;
+    public String toJSON(){
+        return "{" +
+                "\"id\": \"" + id + '\"' +
+                ", \"firstName\": \"" + firstName + '\"' +
+                ", \"lastName\": \"" + lastName + '\"' +
+                ", \"email\": \"" + email + '\"' +
+                ", \"age\": \"" + age + '\"' +
+                ", \"password\": \"" + password + '\"' +
+                ", \"address\": \"" + address + '\"' +
+                ", \"phone\": \"" + phone + '\"' +
+                ", \"bio\": \"" + bio + '\"' +
+                '}';
+    }
+}
